b15b88501d1ff4b938a029b657ae2588b0beb260
Joshua Robinson <joshua@codeadventure.uk>
2024-07-09 08:03:01 +0100

    Refactored multiplesOf3or5 to remove unnecessary lines

--- katas/multiplesOf3or5.js
+++ katas/multiplesOf3or5.js
@@ -1,15 +1,11 @@
 function multiplesOf3or5(num) {
     let sum = 0;
-    if (num < 3){
-        return 0;
-    } else {
-        for(let i = 0; i < num; i++){
-            if (i % 3 === 0 || i % 5 === 0){
-                sum += i;
-            }
+    for(let i = 0; i < num; i++){
+        if (i % 3 === 0 || i % 5 === 0){
+            sum += i;
         }
-        return sum;
     }
+    return sum;
 }
 
 module.exports = { multiplesOf3or5 }
